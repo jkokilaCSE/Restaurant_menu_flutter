e8d5cb6c60ff60d1cdd445fb331d3f00ec9cf1db
Kokila <jkokila2810@gmail.com>
2025-01-30 22:24:57 +0530

    Restaurant menu done using Column and learned widgets

---
+++ lib/main.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Restaurant Menu",style: TextStyle(color: Colors.black,),),
+          backgroundColor: Colors.orange,
+           ),
+           body:ListView(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           ),
+           Container(
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0,5 ),
+                  color:Colors.black26,),
+            ]),
+            height: 90,
+            width: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chicken Noodles",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                Text("Delicious Chicken noodles with spices"),
+                Text("29.05",style: TextStyle(fontWeight: FontWeight.bold),)
+              ],
+            ),
+           )
+            ],
+            
+            
+           ),
+
+       
+      ),
+    );
+  }
+}
